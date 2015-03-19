03286ddc67fa89a81bb322a9941786f09110dbbe
kamuniaft <kliamshin.v@gmail.com>
2015-03-19 23:53:36 +0600

    composer fixes

--- assets/views/main.php
+++ assets/views/main.php
@@ -3,12 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title>СибНГП</title>
-        <link href="/components/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
-        <link href="/components/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
-        <link href="/strelec.css" rel="stylesheet">        
-        <script src="/components/jquery/jquery.min.js"></script>
-        <script src="/components/bootstrap/js/bootstrap.min.js"></script>       
+        <link href="components/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="components/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />     
+        <script src="components/jquery/jquery.min.js"></script>
+        <script src="components/bootstrap/js/bootstrap.min.js"></script>       
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     </head>
     <body>
