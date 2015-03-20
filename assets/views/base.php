9e77d369ecefcc14303d87ce2444e3356ed6fba2
kamuniaft <kliamshin.v@gmail.com>
2015-03-20 08:14:48 +0600

    some basic styles added

--- assets/views/base.php
+++ assets/views/base.php
@@ -1,1 +1,20 @@
-<h2><?php echo $message;?></h2>
+<header class="navbar navbar-default">
+    <div class="container-fluid">
+        <div id="navbar">
+            <ul class="navbar-nav nav">
+                <li><a href="#">Lorem</a></li>
+                <li><a href="#">Ipsum</a></li>
+                <li><a href="#">Dolor</a></li>
+                <li><a href="#">Sit</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+<section id="content">
+    <div id="left">
+        <?= $message ?>
+    </div>
+    <div id="right">
+        <?= $message ?>
+    </div>
+</section>
--- assets/views/main.php
+++ assets/views/main.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+    <head>                
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>СибНГП</title>
         <link href="components/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
         <link href="components/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="sibngp.css" rel="stylesheet">
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />     
         <script src="components/jquery/jquery.min.js"></script>
         <script src="components/bootstrap/js/bootstrap.min.js"></script>       
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     </head>
     <body>
+        <section id="main" class="container">
             <!-- Here is where we include a subtemplate -->
             <?php include($subview.'.php');?>
+        </section>
     </body>
 </html>
