2874363035d9b2ee5e08c3c2794c1349991a4968
kamuniaft <kliamshin.v@gmail.com>
2015-03-22 14:13:21 +0600

    slick carousel plugin added

--- assets/views/main.php
+++ assets/views/main.php
@@ -8,10 +8,12 @@
         <link href="components/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
         <link href="components/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="sibngp.css" rel="stylesheet">
+        <link href="assets/slick/slick.css" rel="stylesheet">
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />     
         <script src="components/jquery/jquery.min.js"></script>
         <script src="components/bootstrap/js/bootstrap.min.js"></script>       
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+        <script src="assets/slick/slick.min.js"></script>
     </head>
     <body>
         <section id="main" class="container">
